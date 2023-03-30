745e008f9faa286d28f7a4d115fb456e7e94ef08
somerandomguy64 <faizan163123@gmail.com>
2023-03-30 11:50:34 +0100

    task 6 added and refactored tests, changed models

--- models/getComments.model.js
+++ models/getComments.model.js
@@ -1,14 +1,30 @@
 const db = require("../db/connection");
 
 exports.selectComments = (review_id) => {
-  return db.query("SELECT * FROM comments WHERE comments.review_id=$1 ORDER BY created_at DESC", [review_id]).then(({ rows }) => {
-    const comments = rows;
-    if (comments.length === 0) {
-      return Promise.reject({
-        status: 404,
-        msg: `No comments found for review_id: ${review_id}`,
-      });
-    }
-    return comments;
-  });
+  return db
+    .query("SELECT * FROM reviews WHERE review_id = $1", [review_id])
+    .then(({ rows }) => {
+      const review = rows[0];
+      if (!review) {
+        return Promise.reject({
+          status: 404,
+          msg: `No review found for review_id: ${review_id}`,
+        });
+      }
+      return db
+        .query(
+          "SELECT * FROM comments WHERE comments.review_id=$1 ORDER BY created_at DESC",
+          [review_id]
+        )
+        .then(({ rows }) => {
+          const comments = rows;
+          if (comments.length === 0) {
+            return Promise.reject({
+              status: 204,
+              msg: `No comments found for review_id: ${review_id}`,
+            });
+          }
+          return comments;
+        });
+    });
 };
