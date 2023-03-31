8f7ddc1ce69a1ec3e643b0c1c2d7cfec075c5f1d
somerandomguy64 <faizan163123@gmail.com>
2023-03-31 15:51:55 +0100

    task 12 done

--- models/getReviewByID.model.js
+++ models/getReviewByID.model.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 
 exports.selectReviewByID = (review_id) => {
   return db
-    .query("SELECT * FROM reviews WHERE review_id = $1", [review_id])
+    .query("SELECT reviews.*, COUNT(comments.review_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id", [review_id])
     .then(({ rows }) => {
       const review = rows[0];
       if (!review) {
